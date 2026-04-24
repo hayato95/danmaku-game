bb679a4c940287c96aa0031e0ed162e6004e32d4
hayato95 <hayato.0905o@gmail.com>
2026-04-24 16:52:39 +0900

    feat:EnemyHomingの新規作成

---
+++ Assets/Scripts/EnemyHoming.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyHoming : Enemy
+{
+
+    private Transform playerTransform;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerTransform = FindFirstObjectByType<PlayerController>().transform;
+    }
+
+    public override void Move()
+    {
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+}
