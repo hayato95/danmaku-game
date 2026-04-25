e13fde820978321c42a98b53a4b926a2eba0c471
hayato95 <hayato.0905o@gmail.com>
2026-04-25 18:43:05 +0900

    feat:Boss.csの骨格作成

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Boss : Enemy
+{
+    [SerializeField] private int MaxHP = 100;
+    private int phaseNumber = 1;
+
+    private void Start()
+    {
+      enemyHP = MaxHP;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+        PhaseManager();
+
+    }
+
+    protected void PhaseManager()
+    {
+        if ((enemyHP * 100) / MaxHP <= 66 && phaseNumber == 1)
+        {
+            phaseNumber = 2;
+        }
+
+        if ((enemyHP * 100) / MaxHP <= 33 && phaseNumber == 2)
+        {
+            phaseNumber = 3;
+        }
+
+    }
+
+    protected override void Move() 
+    {
+        //ˆ—
+    }
+
+    protected override void CheckBoundary()
+    {
+        //ˆ—
+    }
+
+}
