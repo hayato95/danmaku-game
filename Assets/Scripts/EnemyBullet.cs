be41dd4b2aa7120378a5f9f061fa37ba42ced20f
hayato95 <hayato.0905o@gmail.com>
2026-04-30 17:26:39 +0900

    fix:弾の画面外処理を修正

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
-    [SerializeField] private float bulletSpeed = 4f;
-
     void Update()
     {
-        transform.Translate(Vector2.down * bulletSpeed * Time.deltaTime);
 
-        if (transform.position.y < -6f)
+        if (Mathf.Abs(transform.position.x) > 3f || Mathf.Abs(transform.position.y) > 6f)
         {
             Destroy(gameObject);
         }
